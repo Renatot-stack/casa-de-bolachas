e1b45491cfbfaf12f02bf1c7633538fbaa1a4796
Renatot-stack <renatogamertg1@gmail.com>
2026-04-09 20:11:46 -0300

    Adicionado o frame do histórico

--- ui.py
+++ ui.py
@@ -1,34 +1,52 @@
 import customtkinter as ctk
-import CTkMessagebox
+# import CTkMessagebox
 import database
 
 # Cores
 marrom_c = "#E8AF76"
 marrom = "#BC7F43"
 marrom_e = "#43280C"
+marrom_bonito = "#724100"
 
 # Inicia o Banco
 database.init_db()
 
 # Funções:
 
+def _pegar_foco(window = ctk.CTkToplevel):
+    window.lift()
+    window.focus_force()
+    window.grab_set()
+
+def _main_frame_atualizar(_funcao):
+    for i, widget in enumerate(main_frame.winfo_children()):
+        if i != 0:
+            widget.destroy()
+    _funcao()
+        
 def _registrar_produto():
-    level_registrar = ctk.CTkToplevel(janela)
-    level_registrar.title('Registrar Produtos')
-    level_registrar.focus()
+    title_main_frame.configure(text='Registrar Produtos')
+    frame = ctk.CTkFrame(main_frame)
+    frame.grid(row=1, column=0, sticky='nsew')
 
-    entry_nome = ctk.CTkEntry(level_registrar, placeholder_text="Nome...")
-    entry_nome.grid(row=0, column=0, pady=5, padx=5)
+    frame.columnconfigure(0, weight=1)
+    frame.columnconfigure(1, weight=1)
+    
+    entry_nome = ctk.CTkEntry(frame, placeholder_text="Nome...")
+    entry_nome.grid(row=1, column=0, pady=5, padx=5)
 
-    ctk.CTkLabel(level_registrar, text="Produto no quilo:").grid(row=0, column=1, padx=5, pady=5)
-    exige_peso = ctk.CTkOptionMenu(level_registrar, values=['Sim', 'Não'])
-    exige_peso.grid(row=0, column=2, padx=2, pady=5)
+    ctk.CTkLabel(frame, text="Produto no quilo:").grid(row=2, column=0, padx=5, pady=5)
+    exige_peso = ctk.CTkOptionMenu(frame, values=['Sim', 'Não'])
+    exige_peso.grid(row=2, column=1, padx=2, pady=5)
 
-    entry_preco = ctk.CTkEntry(level_registrar, fg_color=marrom_c, placeholder_text='Preço...')
-    entry_preco.grid(row=0, column=3)
+    entry_preco = ctk.CTkEntry(frame, placeholder_text='Preço...')
+    entry_preco.grid(row=1, column=1)
+    
+    button_enviar = ctk.CTkButton(down_frame, text='Enviar')
+    button_enviar.grid(row=0, column=1, sticky='e', padx=10, pady=10)
 
 # Janela principal
-janela = ctk.CTk(fg_color="#E8AF76")
+janela = ctk.CTk(fg_color=marrom_c)
 janela.title('Casa de Bolachas - Sistema de Estoque')
 janela.minsize(450, 300)
 
@@ -36,15 +54,22 @@ janela.minsize(450, 300)
 janela.columnconfigure(0, weight=1)
 janela.rowconfigure(1, weight=1)
 
+# Header
+
+header_frame = ctk.CTkFrame(janela)
+header_frame.grid(row=0, column=0, pady=5, padx=5, sticky='we', columnspan=2)
+header_frame.columnconfigure(0, weight=1)
+ctk.CTkLabel(header_frame, text='🍪 Casa de Bolachas 🍪', font=('Arial', 20)).grid(row=0, column=0, padx=5, pady=5, sticky='ew', columnspan=2)
+
 # Div Casa de Bolachas
+
 frame_bolacha = ctk.CTkFrame(janela, fg_color=marrom)
 frame_bolacha.grid(row=1, column=0, padx=5, pady=5, sticky='nsew')
-ctk.CTkLabel(janela, text='🍪 Casa de Bolachas').grid(row=0, column=0, padx=5, pady=5, sticky='new')
 
 # Frame Bolacha configuração
 frame_bolacha.columnconfigure(0, weight=1)
 
-# Div De Cima / header / cabeçalho
+# Div De Cima
 
 top_frame = ctk.CTkFrame(frame_bolacha)
 top_frame.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
@@ -53,7 +78,7 @@ top_frame.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
 top_frame.columnconfigure(2, weight=1)
 
 # Botão registrar produto 
-registrar_prod = ctk.CTkButton(top_frame, text='Registrar Produto', fg_color=marrom_e, command=_registrar_produto)
+registrar_prod = ctk.CTkButton(top_frame, text='Registrar Produto', fg_color=marrom_e, command=lambda: _main_frame_atualizar(_registrar_produto))
 registrar_prod.grid(row=0, column=0, padx=10, pady=10)
 
 # Botão novo pedido
@@ -69,17 +94,26 @@ ver_lucro.grid(row=0, column=2, padx=10, pady=10, sticky='e')
 frame_bolacha.rowconfigure(1, weight=1)
 
 main_frame = ctk.CTkFrame(frame_bolacha)
+main_frame.columnconfigure(0, weight=1)
+main_frame.rowconfigure(1, weight=1)
 
 main_frame.grid(row=1, column=0, padx=5, pady=5, sticky='nsew')
 
-# Texto de teste
-ctk.CTkLabel(main_frame, text='Sistema de Gerenciamento de lucros, estoque, pedidos...').grid()
+# Título do frame
+title_main_frame = ctk.CTkLabel(main_frame, text='Sistema de Gerenciamento de lucros, estoque, pedidos...', text_color='white', bg_color=marrom_bonito, corner_radius=10)
+title_main_frame.grid(row=0, column=0, sticky='ew')
 
 # Div de baixo / footer / rodape
 
 down_frame = ctk.CTkFrame(frame_bolacha)
+down_frame.columnconfigure(1, weight=1)
 
 down_frame.grid(row=2, column=0, padx=5, pady=5, sticky='ew')
+
+# Div do histórico
+
+historico_frame = ctk.CTkFrame(janela, width=300)
+historico_frame.grid(row=1, column=1, sticky='nse', pady=5, padx=5)
 
 # Texto de teste
 ctk.CTkLabel(down_frame, text='All rights reserved').grid()
